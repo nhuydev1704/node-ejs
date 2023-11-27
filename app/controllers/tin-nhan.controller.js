de34cae5c65885ec1d217f92fb37b5f8a99025a6
nhuydev1704 <khanhandli@gmail.com>
2023-11-27 17:30:19 +0700

    Feat: detail message

--- app/controllers/tin-nhan.controller.js
+++ app/controllers/tin-nhan.controller.js
@@ -1,85 +1,86 @@
-"use strict";
-const { BangDia, QLBD } = require("../models/bangDia.model");
-const { QLTN, TinNhan } = require("../models/tin-nhan.modal");
-const { User, QLUser } = require("../models/user.model");
+'use strict';
+const { BangDia, QLBD } = require('../models/bangDia.model');
+const { QLTN, TinNhan } = require('../models/tin-nhan.modal');
+const { User, QLUser } = require('../models/user.model');
 
 class TinNhanCtrl {
-  index(req, res) {
-    res.locals.deleted = req.query.deleted;
-    const content = req.query.content;
-    QLTN.getAll(content, (err, data) => {
-      console.log(
-        "🚀 ~ file: tin-nhan.controller.js:13 ~ TinNhanCtrl ~ QLTN.getAll ~ data:",
-        data
-      );
-      if (err) res.redirect("/500");
-      else res.render("tin-nhan/index", { data: data });
-    });
-  }
+    index(req, res) {
+        res.locals.deleted = req.query.deleted;
+        const content = req.query.content;
+        QLTN.getAll(content, (err, data) => {
+            console.log('🚀 ~ file: tin-nhan.controller.js:13 ~ TinNhanCtrl ~ QLTN.getAll ~ data:', data);
+            if (err) res.redirect('/500');
+            else res.render('tin-nhan/index', { data });
+        });
+    }
 
-  taoTin(req, res) {
-    QLUser.getAll(req, (err, data) => {
-      if (err) res.redirect("/500");
-      else res.render("tin-nhan/create", { data: data });
-    });
-  }
+    taoTin(req, res) {
+        QLUser.getAll(req, (err, data) => {
+            if (err) res.redirect('/500');
+            else res.render('tin-nhan/create', { data });
+        });
+    }
 
-  showFormCreate(req, res) {
-    res.locals.status = req.query.status;
-    res.render("bangDia/create");
-  }
+    detail(req, res) {
+        QLUser.getAll(req, (err, data) => {
+            if (err) res.redirect('/500');
+            else res.render('tin-nhan/detail', { data });
+        });
+    }
 
-  create(req, res) {
-    console.log(
-      "🚀 ~ file: tin-nhan.controller.js:34 ~ TinNhanCtrl ~ create ~ req.body:",
-      req.body
-    );
-    const newTinNhan = new TinNhan(req.body);
-    QLTN.insert(newTinNhan, (err, result) => {
-      console.log(result);
-      res.redirect("/tin-nhan");
-    });
-  }
+    showFormCreate(req, res) {
+        res.locals.status = req.query.status;
+        res.render('bangDia/create');
+    }
 
-  showFormEdit(req, res) {
-    res.locals.status = req.query.status;
-    QLBD.getById(req.params.id, (err, result) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.redirect("/404");
-        } else {
-          res.redirect("/500");
-        }
-      } else res.render("bangDia/edit", { bangDia: result });
-    });
-  }
+    create(req, res) {
+        console.log('🚀 ~ file: tin-nhan.controller.js:34 ~ TinNhanCtrl ~ create ~ req.body:', req.body);
+        const newTinNhan = new TinNhan(req.body);
+        QLTN.insert(newTinNhan, (err, result) => {
+            console.log(result);
+            res.redirect('/tin-nhan');
+        });
+    }
 
-  update(req, res) {
-    const newBangDia = new BangDia(req.body);
-    QLBD.update(req.params.id, newBangDia, (err, result) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.redirect("/404");
-        } else {
-          res.redirect("/500");
-        }
-      } else {
-        res.redirect("/bangDia");
-      }
-    });
-  }
+    showFormEdit(req, res) {
+        res.locals.status = req.query.status;
+        QLBD.getById(req.params.id, (err, result) => {
+            if (err) {
+                if (err.kind === 'not_found') {
+                    res.redirect('/404');
+                } else {
+                    res.redirect('/500');
+                }
+            } else res.render('bangDia/edit', { bangDia: result });
+        });
+    }
 
-  delete(req, res) {
-    QLBD.delete(req.params.id, (err, result) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.redirect("/404");
-        } else {
-          res.redirect("/500");
-        }
-      } else res.redirect("/bangDia?deleted=true");
-    });
-  }
+    update(req, res) {
+        const newBangDia = new BangDia(req.body);
+        QLBD.update(req.params.id, newBangDia, (err, result) => {
+            if (err) {
+                if (err.kind === 'not_found') {
+                    res.redirect('/404');
+                } else {
+                    res.redirect('/500');
+                }
+            } else {
+                res.redirect('/bangDia');
+            }
+        });
+    }
+
+    delete(req, res) {
+        QLBD.delete(req.params.id, (err, result) => {
+            if (err) {
+                if (err.kind === 'not_found') {
+                    res.redirect('/404');
+                } else {
+                    res.redirect('/500');
+                }
+            } else res.redirect('/bangDia?deleted=true');
+        });
+    }
 }
 
 module.exports = new TinNhanCtrl();
--- app/routers/tin-nhan.route.js
+++ app/routers/tin-nhan.route.js
@@ -1,11 +1,12 @@
-const router = require("express").Router();
-const TinNhanCtrl = require("../controllers/tin-nhan.controller");
-const { checkSession } = require("../utils");
+const router = require('express').Router();
+const TinNhanCtrl = require('../controllers/tin-nhan.controller');
+const { checkSession } = require('../utils');
 
 module.exports = (app) => {
-  router.get("/", TinNhanCtrl.index);
-  router.get("/tao-tin", TinNhanCtrl.taoTin);
-  router.post("/tao-tin", TinNhanCtrl.create);
+    router.get('/', TinNhanCtrl.index);
+    router.get('/tao-tin', TinNhanCtrl.taoTin);
+    router.post('/tao-tin', TinNhanCtrl.create);
+    router.get('/chi-tiet', TinNhanCtrl.detail);
 
-  app.use("/tin-nhan", router);
+    app.use('/tin-nhan', router);
 };
